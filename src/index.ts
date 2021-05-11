294e64110b3f39ddf92143ffc3ec1a5b6d25cc55
Nate <choseongho93@naver.com>
2021-05-11 13:17:30 +0900

    interface add study

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+interface Human{
+    name : string;
+    age : number;
+    gender : string;
+}
 
+const person = {
+    name : "nate",
+    age : 25,
+    gender : "male"
+}
 
-const sayHi = (name:string, age:number, gender:string) : string =>{
-    return `Hello ${name}, you are ${age},  you are a ${gender} !`;
+const sayHi = (person: Human) : string =>{
+    return `Hello ${person.name}, you are ${person.age},  you are a ${person.gender} !`;
 } 
 
-console.log(sayHi("nate", 22, "male"));
+console.log(sayHi(person));
+// console.log(sayHi("nate", 22, "male"));
 
 export{};
