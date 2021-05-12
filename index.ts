26b78f3d8b6c0a43f8efe410f13d8bc7d9378b54
nate <choseongho93@naver.com>
2021-05-12 21:16:44 +0900

    Block first add

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const name = "nate",
     age = 29,
-    gender = "male";
+    gender = "males";
 
 const sayHi = (name, age, gender) =>{
     console.log(`Hello ${name}, you are ${age},  you are a ${gender}`);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
-interface Human{
-    name : string;
-    age : number;
-    gender : string;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor
+        (
+            index: number,
+            hash: string,
+            previousHash: string,
+            data: string,
+            timestamp: number
+        ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name : "nate",
-    age : 25,
-    gender : "male"
-}
+const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-const sayHi = (person: Human) : string =>{
-    return `Hello ${person.name}, you are ${person.age},  you are a ${person.gender} !`;
-} 
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(person));
-// console.log(sayHi("nate", 22, "male"));
+console.log(blockchain);
 
-export{};
+
+export { };
