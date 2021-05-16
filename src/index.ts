1df7cfe8dd3b48da8b19249f16da3b57b0d227df
Nate <choseongho93@naver.com>
2021-05-16 15:24:14 +0900

    blockValid add

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ class Block {
         ): string =>
             CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
-    // static validateStructure = (aBlock: Block): Boolean => typeof aBlock.index === "number" && typeof aBlock.hash === "string" && typeof aBlock.previousHash === "string" && typeof aBlock.timestamp === "number" && typeof aBlock.data === "string";
+    static validateStructure = (aBlock: Block): Boolean => typeof aBlock.index === "number" && typeof aBlock.hash === "string" && typeof aBlock.previousHash === "string" && typeof aBlock.timestamp === "number" && typeof aBlock.data === "string";
 
 
 
@@ -64,16 +64,15 @@ const createNewBlock = (data: string): Block => {
     return newBlock;
 }
 
-console.log(createNewBlock("hello"));
-console.log(createNewBlock("bye"));
-// const isBlockYalid = (candidateBlock: Block, previousBlock: Block): boolean => {
-//     if (!Block.validateStructure(candidateBlock)) {
-//         return false;
-//     }else if (previousBlock.index+1 !=== candidateBlock.index){
-//         return false;
-//     }else if(previousBlock.hash !=== candidateBlock.previousHash){
-//         return false;
-//     }
-// }
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+      return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+      return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+      return false;
+    }
+}
+
 
 export { };
