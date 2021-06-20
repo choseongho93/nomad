e60f1d0003df6246cb76e71964007432ed220859
Nate <choseongho93@naver.com>
2021-06-20 12:49:40 +0900

    Extending the Schema

--- graphql_api/graphql/resolvers.js
+++ graphql_api/graphql/resolvers.js
@@ -1,6 +1,14 @@
+const nicolas = {
+    name: "Nicolas",
+    age: 18,
+    gender: "female"
+  };
+  
+
+
 const resolvers = {
     Query: {
-      name: () => "nicolas"
+        person: () => nicolas
     }
   };
   
